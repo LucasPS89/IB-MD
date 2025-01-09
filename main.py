8ddcf6423febbd0d42073aa61ce8d78f3f693b89
Lucas Pires <souza.lucas@outlook.com>
2025-01-08 23:29:41 -0300

    Create dynamoDB table for each day

--- main.py
+++ main.py
@@ -1,49 +1,63 @@
 import ib_async
 import boto3 
-import json
-from decimal import Decimal
 import jsonpickle
+from datetime import datetime
+import time
 
 #ib_async.util.logToConsole("DEBUG")
 ib = ib_async.IB()
 dynamodb = boto3.resource('dynamodb')
+table_name = f"FutureTicks1-{datetime.now().strftime("%Y%m%d")}"
 
 def create_dynamo_table():
-    table = dynamodb.create_table(
-        TableName='FutureTicks1',
-        KeySchema=[
-            {
-                'AttributeName': 'contract',
-                'KeyType': 'HASH'
-            },
-            {
-                'AttributeName': 'time',
-                'KeyType': 'RANGE'
-            }
-        ],
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'contract',
-                'AttributeType': 'S'
-            },
-            {
-                'AttributeName': 'time',
-                'AttributeType': 'S'
-            },
-        ],
-        ProvisionedThroughput={
-            'ReadCapacityUnits': 5,
-            'WriteCapacityUnits': 5
-        }
-    )
+    table_names = [table.name for table in dynamodb.tables.all()]
 
-    print("Table status:", table.table_status)
+    if not table_name in table_names:
+        table = dynamodb.create_table(
+            TableName=table_name,
+            KeySchema=[
+                {
+                    'AttributeName': 'contract',
+                    'KeyType': 'HASH'
+                },
+                {
+                    'AttributeName': 'time',
+                    'KeyType': 'RANGE'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'contract',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'time',
+                    'AttributeType': 'S'
+                },
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5
+            }
+        )
+        time.sleep(2)
+    else:
+        table = dynamodb.Table(table_name)
+
+    #Wait for table to be created    
+    while True:
+        table = dynamodb.Table(table_name)
+        print(f"Table {table_name} status:", table.table_status)
+        time.sleep(1)
+        if table.table_status == 'ACTIVE':
+            break
 
 def wait_for_market_data(ticker: list[ib_async.ticker.Ticker]):
       """print tickers as they arrive"""
       for t in ticker:
             #print(t)
-            dynamodb.Table('FutureTicks1').put_item(Item={
+
+            dynamodb.Table(table_name).put_item(Item={
                 'contract':f"{t.contract.exchange}_{t.contract.symbol}_{t.contract.localSymbol}",
                 'time':t.time.strftime("%m/%d/%Y, %H:%M:%S.%f"),
                 'ticker': jsonpickle.encode(t)
@@ -63,7 +77,7 @@ def wait_for_market_data(ticker: list[ib_async.ticker.Ticker]):
       #             " last:", t.last, " lastSize:", t.lastSize, " volume:", t.volume, " open:", t.open, " high:", t.high, " low:", t.low, " close:", t.close)
 
 def main():
-    #create_dynamo_table()
+    create_dynamo_table()
 
     #Connect to IB
     ib.connect(port=4002) #DEMO
